8848768bb20d118cb293caea1e478385bb80db5e
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-10 17:05:45 +0300

    Add sortingAlgorithms class library

---
+++ src/sortingAlgorithms/BubbleSortExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+namespace sortingAlgorithms
+{
+    public static class BubbleSortExtension
+    {
+        public static void BubbleSort(this IList list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = 0; j < list.Count; j++)
+                {
+
+                }
+            }
+        }
+    }
+}
