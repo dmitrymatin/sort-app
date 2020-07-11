62e27959f2083346ede7acac77df67b2f9c1a89e
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-11 20:40:04 +0300

    Implement Bubble sort algorithm for ascending order sorting

--- src/sortingAlgorithms/BubbleSortExtension.cs
+++ src/sortingAlgorithms/BubbleSortExtension.cs
@@ -1,16 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static sortingAlgorithms.Helpers;
 
 namespace sortingAlgorithms
 {
     public static class BubbleSortExtension
     {
-        public static void BubbleSort(this IList list)
+        public static void BubbleSort<T>(this IList<T> list, Comparer<T> comparer = null, SortingOrder order = SortingOrder.Ascending)
         {
-            for (int i = 0; i < list.Count; i++)
-            {
-                for (int j = 0; j < list.Count; j++)
-                {
+            comparer ??= Comparer<T>.Default;
 
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    var current = j;
+                    var next = j + 1;
+                    if (comparer.Compare(list[current], list[next]) > 0)
+                    {
+                        list.Swap(current, next);
+                    }
                 }
             }
         }
