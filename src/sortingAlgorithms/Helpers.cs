3d1f696987363246cbdbd807990a234f16f54381
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-11 20:38:17 +0300

    Create Swap<T>() as an extension method for IList<T> objects. Add Sorting order enum

---
+++ src/sortingAlgorithms/Helpers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace sortingAlgorithms
+{
+    public static class Helpers
+    {
+        public static void Swap<T>(this IList<T> list, int indexA, int indexB)
+        {
+            T temp = list[indexA];
+            list[indexA] = list[indexB];
+            list[indexB] = temp;
+        }
+    }
+
+    public enum SortingOrder
+    {
+        Ascending = 0,
+        Descending = 1
+    }
+}
