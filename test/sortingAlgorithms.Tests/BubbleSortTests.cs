f9f447147d3e4220d3a3366560c4f1d3e2112987
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-11 21:34:32 +0300

    Create Bubble Sort basic performance time test

--- test/sortingAlgorithms.Tests/BubbleSortTests.cs
+++ test/sortingAlgorithms.Tests/BubbleSortTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace sortingAlgorithms.Tests
@@ -20,6 +21,42 @@ namespace sortingAlgorithms.Tests
 
             // assert
             Assert.Equal(sampleArray, arrayForBubbleSort);
+        }
+
+
+        [Fact]
+        public void BubbleSortIsFast()
+        {
+            // arrange
+            const int size = 100;
+            int[] arr = new int[size];
+            var ran = new Random();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                arr[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrBubble = new int[size];
+            arr.CopyTo(arrBubble, 0);
+
+            long arrTime, arrBubbleTime;
+            var stopwatch = new Stopwatch();
+
+            // act
+            stopwatch.Start();
+            Array.Sort(arr);
+            stopwatch.Stop();
+            arrTime = stopwatch.ElapsedTicks;
+
+            stopwatch.Restart();
+            arrBubble.BubbleSort();
+            stopwatch.Stop();
+            arrBubbleTime = stopwatch.ElapsedTicks;
+
+            var diff = ((arrBubbleTime - arrTime) / (double)arrTime) * 100;
+
+            // assert
+            Assert.InRange(diff, -100, 12000);
         }
     }
 }
