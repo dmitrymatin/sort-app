5cdd538e4207b42bc36a5c0e5f7f5177b39e25e5
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-12 16:43:32 +0300

    Add BubbleSortDescending and a performance test

--- src/sortingAlgorithms/BubbleSortExtension.cs
+++ src/sortingAlgorithms/BubbleSortExtension.cs
@@ -7,7 +7,7 @@ namespace sortingAlgorithms
 {
     public static class BubbleSortExtension
     {
-        public static void BubbleSort<T>(this IList<T> list, Comparer<T> comparer = null, SortingOrder order = SortingOrder.Ascending)
+        public static void BubbleSortAscending<T>(this IList<T> list, Comparer<T> comparer = null, SortingOrder order = SortingOrder.Ascending)
         {
             comparer ??= Comparer<T>.Default;
 
@@ -18,6 +18,24 @@ namespace sortingAlgorithms
                     var current = j;
                     var next = j + 1;
                     if (comparer.Compare(list[current], list[next]) > 0)
+                    {
+                        list.Swap(current, next);
+                    }
+                }
+            }
+        }
+
+        public static void BubbleSortDescending<T>(this IList<T> list, Comparer<T> comparer = null, SortingOrder order = SortingOrder.Descending)
+        {
+            comparer ??= Comparer<T>.Default;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                for (int j = 0; j < list.Count - i - 1; j++)
+                {
+                    var current = j;
+                    var next = j + 1;
+                    if(comparer.Compare(list[current], list[next]) < 0)
                     {
                         list.Swap(current, next);
                     }
--- test/sortingAlgorithms.Tests/BubbleSortTests.cs
+++ test/sortingAlgorithms.Tests/BubbleSortTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using System.Diagnostics;
 using Xunit;
 
@@ -9,7 +10,7 @@ namespace sortingAlgorithms.Tests
     {
         [Theory]
         [InlineData(new object[] { new int[] { 3, 2, 4, 1 } })]
-        public void BubbleSortGivesCorrectOutput(int[] sampleArray)
+        public void BubbleSortAscendingGivesCorrectOutput(int[] sampleArray)
         {
             // arrange
             int[] arrayForBubbleSort = new int[sampleArray.Length];
@@ -17,30 +18,48 @@ namespace sortingAlgorithms.Tests
 
             // act
             Array.Sort(sampleArray);
-            arrayForBubbleSort.BubbleSort(order: SortingOrder.Ascending);
+            arrayForBubbleSort.BubbleSortAscending(order: SortingOrder.Ascending);
 
             // assert
             Assert.Equal(sampleArray, arrayForBubbleSort);
         }
 
-
-        [Fact]
-        public void BubbleSortIsFast()
+        [Theory]
+        [InlineData(new object[] { new int[] { 3, 2, 4, 1 } })]
+        public void BubbleSortDescendingGivesCorrectOutput(int[] sampleArray)
         {
             // arrange
-            const int size = 100;
-            int[] arr = new int[size];
+            int[] arrayForReverseBubbleSort = new int[sampleArray.Length];
+            sampleArray.CopyTo(arrayForReverseBubbleSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray, new Comparison<int>(
+                (i1, i2) => i2.CompareTo(i1))
+            );
+            arrayForReverseBubbleSort.BubbleSortDescending(order: SortingOrder.Descending);
+
+            // assert
+            Assert.Equal(sampleArray, arrayForReverseBubbleSort);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void BubbleSortIsFast(int arrSize)
+        {
+            // arrange
+            int[] arr = new int[arrSize];
             var ran = new Random();
             for (var i = 0; i < arr.Length; i++)
             {
                 arr[i] = ran.Next(int.MinValue, int.MaxValue);
             }
 
-            int[] arrBubble = new int[size];
+            int[] arrBubble = new int[arrSize];
             arr.CopyTo(arrBubble, 0);
 
             long arrTime, arrBubbleTime;
             var stopwatch = new Stopwatch();
+
 
             // act
             stopwatch.Start();
@@ -49,14 +68,51 @@ namespace sortingAlgorithms.Tests
             arrTime = stopwatch.ElapsedTicks;
 
             stopwatch.Restart();
-            arrBubble.BubbleSort();
+            arrBubble.BubbleSortAscending();
             stopwatch.Stop();
             arrBubbleTime = stopwatch.ElapsedTicks;
 
             var diff = ((arrBubbleTime - arrTime) / (double)arrTime) * 100;
 
+
             // assert
             Assert.InRange(diff, -100, 12000);
+        }
+
+        [Theory]
+        [InlineData(100)]
+        public void BubbleSortIsEquallyFastForAllOrders(int arrSize)
+        {
+            // arrange
+            int[] arrAsc = new int[arrSize];
+            var ran = new Random();
+            for (var i = 0; i < arrAsc.Length; i++)
+            {
+                arrAsc[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrDesc = new int[arrSize];
+            arrAsc.CopyTo(arrDesc, 0);
+
+            long ascTime, descTime;
+            var stopwatch = new Stopwatch();
+
+
+            // act
+            stopwatch.Start();
+            arrAsc.BubbleSortAscending();
+            stopwatch.Stop();
+            ascTime = stopwatch.ElapsedTicks;
+
+            stopwatch.Restart();
+            arrDesc.BubbleSortDescending();
+            stopwatch.Stop();
+            descTime = stopwatch.ElapsedTicks;
+
+            var diff = ((descTime - ascTime) / (double)ascTime) * 100;
+
+            // assert
+            Assert.InRange(diff, -5, 5);
         }
     }
 }
