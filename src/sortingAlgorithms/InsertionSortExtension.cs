bf55d93ccc19f02842ff4dfa118f632a5d32db68
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-12 21:12:47 +0300

    Add initial InsertionSort implementation

---
+++ src/sortingAlgorithms/InsertionSortExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sortingAlgorithms
+{
+    public static class InsertionSortExtension
+    {
+        public static void InsertionSortAscending<T>(this IList<T> list, Comparer<T> comparer = null)
+        {
+            comparer ??= Comparer<T>.Default;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                if (comparer.Compare(list[i - 1], list[i]) > 0)      // if previous element is greater than current.
+                {
+                    var current = list[i];
+                    var currentIndex = i;
+                    for (int j = 0; j < currentIndex; j++)           // loop through all elements on the left (those are sorted) until current.
+                    {
+                        if (comparer.Compare(current, list[j]) < 0)  // if current is less than an element in that left section.
+                        {
+                            var moveStartIndex = j;
+                            for (int k = currentIndex; k > moveStartIndex/*list.Count - 1*/; k--) // right move section between element that turned out to be 
+                            {                                                                     // greater than current and current element by one position.
+                                list[k] = list[k - 1];
+                            }
+
+                            list[moveStartIndex] = current;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/sortingAlgorithms.Tests/InsertionSortTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xunit;
+
+namespace sortingAlgorithms.Tests
+{
+    public class InsertionSortTests
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 6, 5, 3, 1, 8, 7, 2, 4 } })]
+        public void InsertionSortAscendingGivesCorrectOutput(int[] sampleArray)
+        {
+            // arrange
+            int[] arrayForInsertionSort = new int[sampleArray.Length];
+            sampleArray.CopyTo(arrayForInsertionSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray);
+            arrayForInsertionSort.InsertionSortAscending();
+
+            // assert
+            Assert.Equal(sampleArray, arrayForInsertionSort);
+        }
+
+        [Theory]
+        [InlineData(10000)]
+        public void InsertionSortAndBubbleSortAreEquallyFast(int arrBubbleSize)
+        {
+            // arrange
+            int[] arrBubbleSort = new int[arrBubbleSize];
+            var ran = new Random();
+            for (var i = 0; i < arrBubbleSort.Length; i++)
+            {
+                arrBubbleSort[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrInsertionSort = new int[arrBubbleSize];
+            arrBubbleSort.CopyTo(arrInsertionSort, 0);
+
+            long arrBubbleSortTime, arrInsertionSortTime;
+            var stopwatch = new Stopwatch();
+
+
+            // act
+            stopwatch.Start();
+            arrBubbleSort.BubbleSortAscending();
+            stopwatch.Stop();
+            arrBubbleSortTime = stopwatch.ElapsedTicks;
+
+            stopwatch.Restart();
+            arrInsertionSort.InsertionSortAscending();
+            stopwatch.Stop();
+            arrInsertionSortTime = stopwatch.ElapsedTicks;
+
+            var diff = ((arrInsertionSortTime - arrBubbleSortTime) / (double)arrBubbleSortTime) * 100; // percentage
+
+
+            // assert
+            Assert.InRange(diff, -30, 30);
+        }
+    }
+}
