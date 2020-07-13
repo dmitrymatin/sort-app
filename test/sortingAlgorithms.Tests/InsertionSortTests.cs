624705596da1eac8a6f201985798a2ac0418dc35
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-13 15:00:08 +0300

    Add InsertionSort functionality test for random numbers. Add performance tests to find out that InsertionSortAscendingInitial() method is slower than the new implementation in InsertionSortAscending()

--- test/sortingAlgorithms.Tests/InsertionSortTests.cs
+++ test/sortingAlgorithms.Tests/InsertionSortTests.cs
@@ -10,6 +10,22 @@ namespace sortingAlgorithms.Tests
     {
         [Theory]
         [InlineData(new object[] { new int[] { 6, 5, 3, 1, 8, 7, 2, 4 } })]
+        public void InsertionSortAscendingInitialGivesCorrectOutput(int[] sampleArray)
+        {
+            // arrange
+            int[] arrayForInsertionSort = new int[sampleArray.Length];
+            sampleArray.CopyTo(arrayForInsertionSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray);
+            arrayForInsertionSort.InsertionSortAscendingInitial();
+
+            // assert
+            Assert.Equal(sampleArray, arrayForInsertionSort);
+        }
+
+        [Theory]
+        [InlineData(new object[] { new int[] { 6, 5, 3, 1, 8, 7, 2, 4 } })]
         public void InsertionSortAscendingGivesCorrectOutput(int[] sampleArray)
         {
             // arrange
@@ -25,34 +41,134 @@ namespace sortingAlgorithms.Tests
         }
 
         [Theory]
-        [InlineData(20000)]
-        public void InsertionSortAndBubbleSortAreEquallyFast(int arrBubbleSize)
+        [InlineData(1000)]
+        public void InsertionSortAscendingGivesCorrectOutputOnRandomArrays(int arrSize)
         {
             // arrange
-            int[] arrBubbleSort = new int[arrBubbleSize];
+            int[] sampleArray = new int[arrSize];
+            var ran = new Random();
+            for (var i = 0; i < sampleArray.Length; i++)
+            {
+                sampleArray[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrayForInsertionSort = new int[arrSize];
+            sampleArray.CopyTo(arrayForInsertionSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray);
+            arrayForInsertionSort.InsertionSortAscending();
+
+            // assert
+            Assert.Equal(sampleArray, arrayForInsertionSort);
+        }
+
+        [Theory]
+        [InlineData(20000)]
+        public void InsertionSortAscendingInitialANDInsertionSortAscendingAreEquallyFast(int arrSize)
+        {
+            // arrange
+            int[] arrInsertionSortInitial = new int[arrSize];
+            var ran = new Random();
+            for (var i = 0; i < arrInsertionSortInitial.Length; i++)
+            {
+                arrInsertionSortInitial[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrInsertionSort = new int[arrSize];
+            arrInsertionSortInitial.CopyTo(arrInsertionSort, 0);
+
+            long insertionSortInitialTime, insertionSortTime;
+            var stopwatch1 = new Stopwatch();
+            var stopwatch2 = new Stopwatch();
+
+
+            // act
+            stopwatch1.Start();
+            arrInsertionSortInitial.InsertionSortAscendingInitial();
+            stopwatch1.Stop();
+            insertionSortInitialTime = stopwatch1.ElapsedTicks;
+
+            stopwatch2.Start();
+            arrInsertionSort.InsertionSortAscending();
+            stopwatch2.Stop();
+            insertionSortTime = stopwatch2.ElapsedTicks;
+
+            var diff = ((insertionSortTime - insertionSortInitialTime) / (double)insertionSortInitialTime) * 100; // percentage
+
+
+            // assert
+            Assert.InRange(diff, -5, 5);
+        }
+
+        [Theory]
+        [InlineData(20000)]
+        public void InsertionSortAscendingANDInsertionSortAscendingInitialAreEquallyFast(int arrSize)
+        {
+            // arrange
+            int[] arrInsertionSort = new int[arrSize];
+            var ran = new Random();
+            for (var i = 0; i < arrInsertionSort.Length; i++)
+            {
+                arrInsertionSort[i] = ran.Next(int.MinValue, int.MaxValue);
+            }
+
+            int[] arrInsertionSortInitial = new int[arrSize];
+            arrInsertionSort.CopyTo(arrInsertionSortInitial, 0);
+
+            long insertionSortTime, insertionSortInitialTime;
+            var stopwatch1 = new Stopwatch();
+            var stopwatch2 = new Stopwatch();
+
+
+            // act
+            stopwatch1.Start();
+            arrInsertionSort.InsertionSortAscending();
+            stopwatch1.Stop();
+            insertionSortTime = stopwatch1.ElapsedTicks;
+
+            stopwatch2.Start();
+            arrInsertionSortInitial.InsertionSortAscendingInitial();
+            stopwatch2.Stop();
+            insertionSortInitialTime = stopwatch2.ElapsedTicks;
+
+            var diff = ((insertionSortInitialTime - insertionSortTime) / (double)insertionSortTime) * 100; // percentage
+
+
+            // assert
+            Assert.InRange(diff, -5, 5);
+        }
+
+        [Theory]
+        [InlineData(20000)]
+        public void InsertionSortInitialAndBubbleSortAreEquallyFast(int arrSize)
+        {
+            // arrange
+            int[] arrBubbleSort = new int[arrSize];
             var ran = new Random();
             for (var i = 0; i < arrBubbleSort.Length; i++)
             {
                 arrBubbleSort[i] = ran.Next(int.MinValue, int.MaxValue);
             }
 
-            int[] arrInsertionSort = new int[arrBubbleSize];
+            int[] arrInsertionSort = new int[arrSize];
             arrBubbleSort.CopyTo(arrInsertionSort, 0);
 
             long arrBubbleSortTime, arrInsertionSortTime;
-            var stopwatch = new Stopwatch();
+            var stopwatch1 = new Stopwatch();
+            var stopwatch2 = new Stopwatch();
 
 
             // act
-            stopwatch.Start();
+            stopwatch1.Start();
             arrBubbleSort.BubbleSortAscending();
-            stopwatch.Stop();
-            arrBubbleSortTime = stopwatch.ElapsedTicks;
+            stopwatch1.Stop();
+            arrBubbleSortTime = stopwatch1.ElapsedTicks;
 
-            stopwatch.Restart();
-            arrInsertionSort.InsertionSortAscending();
-            stopwatch.Stop();
-            arrInsertionSortTime = stopwatch.ElapsedTicks;
+            stopwatch2.Start();
+            arrInsertionSort.InsertionSortAscendingInitial();
+            stopwatch2.Stop();
+            arrInsertionSortTime = stopwatch2.ElapsedTicks;
 
             var diff = ((arrInsertionSortTime - arrBubbleSortTime) / (double)arrBubbleSortTime) * 100; // percentage
 
