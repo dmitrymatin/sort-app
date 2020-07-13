3816ff64f457bbfe6439a3c6c6ff8c68326e03c4
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-13 17:15:49 +0300

    Add initial implementation of SelectionSortAscending()

---
+++ src/sortingAlgorithms/SelectionSortExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sortingAlgorithms
+{
+    public static class SelectionSortExtension
+    {
+        public static void SelectionSortAscending<T>(this IList<T> list, Comparer<T> comparer = null)
+        {
+            comparer ??= Comparer<T>.Default;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                var current = list[i];
+
+                int minElemIndex = i;
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (comparer.Compare(list[j], list[minElemIndex]) < 0)
+                    {
+                        minElemIndex = j;
+                    }
+                }
+                
+                if (i != minElemIndex)
+                {
+                    list.Swap(i, minElemIndex);
+                }
+            }
+        }
+    }
+}
---
+++ test/sortingAlgorithms.Tests/SelectionSortTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace sortingAlgorithms.Tests
+{
+    public class SelectionSortTests
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 6, 5, 3, 1, 8, 7, 2, 4 } })]
+        public void SelectionSortAscendingGivesCorrectOutput(int[] sampleArray)
+        {
+            // arrange
+            int[] arrayForSelectionSort = new int[sampleArray.Length];
+            sampleArray.CopyTo(arrayForSelectionSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray);
+            arrayForSelectionSort.SelectionSortAscending();
+
+            // assert
+            Assert.Equal(sampleArray, arrayForSelectionSort);
+        }
+
+    }
+}
