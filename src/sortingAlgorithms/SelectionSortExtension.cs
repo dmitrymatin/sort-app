855a9c17645436d07ad943d9256aed0acf28fe0e
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-13 17:48:40 +0300

    Create SelectionSortAscending<T> that calls MinIndex<T>() helper method

--- src/sortingAlgorithms/SelectionSortExtension.cs
+++ src/sortingAlgorithms/SelectionSortExtension.cs
@@ -27,5 +27,24 @@ namespace sortingAlgorithms
                 }
             }
         }
+
+        public static void SelectionSortAscending<T>(this IList<T> list, Comparer<T> comparer = null)
+        {
+            comparer ??= Comparer<T>.Default;
+
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int minElemIndex = list.MinIndex(
+                    startIndex: i, 
+                    endIndex: list.Count - 1, 
+                    comparer
+                );
+
+                if (i != minElemIndex)
+                {
+                    list.Swap(i, minElemIndex);
+                }
+            }
+        }
     }
 }
