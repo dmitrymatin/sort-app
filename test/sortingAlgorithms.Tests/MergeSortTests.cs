b46a141747acc281ad065d184565eeedff019108
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-14 15:31:38 +0300

    Add essential test for Merge sort

---
+++ test/sortingAlgorithms.Tests/MergeSortTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace sortingAlgorithms.Tests
+{
+    public class MergeSortTests
+    {
+        [Theory]
+        [InlineData(new object[] { new int[] { 6, 5, 3, 1, 8, 7, 2, 4 } })]
+        public void MergeSortGivesCorrectOutput(int[] sampleArray)
+        {
+            // arrange
+            int[] arrayForMergeSort = new int[sampleArray.Length];
+            sampleArray.CopyTo(arrayForMergeSort, index: 0);
+
+            // act
+            Array.Sort(sampleArray);
+            arrayForMergeSort.MergeSort();
+
+            // assert
+            Assert.Equal(sampleArray, arrayForMergeSort);
+        }
+    }
+}
