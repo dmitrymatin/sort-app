c418364327ec1b1be9dff477140048c88adf65c6
Dmitry Matin <super.dmitry320@yandex.ru>
2020-07-15 15:55:28 +0300

    Add random array generation functionality

--- src/SortApp/AppForm.cs
+++ src/SortApp/AppForm.cs
@@ -23,17 +23,57 @@ namespace SortApp
         }
         private void checkBoxRandom_CheckedChanged(object sender, EventArgs e)
         {
-
+            if (checkBoxRandom.Checked)
+            {
+                labelArrSize.ForeColor = SystemColors.ControlText;
+                textBoxArrSize.Enabled = true;
+                buttonGenArr.Enabled = true;
+            }
+            else
+            {
+                labelArrSize.ForeColor = SystemColors.ControlDark;
+                textBoxArrSize.Enabled = false;
+                buttonGenArr.Enabled = false;
+            }
         }
 
         private void buttonGenArr_Click(object sender, EventArgs e)
         {
+            if (int.TryParse(textBoxArrSize.Text, out int arrSize) && arrSize > 0)
+            {
+                int[] array = GetRandomArray(arrSize);
 
+                dataGridViewData.Rows.Clear();
+                dataGridViewData.Rows.Add(count: arrSize);
+
+                PopulateColumn(array, columnIndex: 0);
+            }
         }
 
         private void buttonSort_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private int[] GetRandomArray(int size)
+        {
+            int[] array = new int[size];
+            var random = new Random();
+            int maxValue = size * 10;
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = random.Next(0, maxValue);
+            }
+            return array;
+        }
+
+        private void PopulateColumn(int[] array, int columnIndex)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                dataGridViewData[columnIndex, i].Value = array[i];
+            }
         }
     }
 }
